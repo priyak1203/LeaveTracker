3c3c857d1d24f770925f96e8b80f91e1d67fb06a
Priydarshini Karanth <kpriya1203@gmail.com>
2024-10-24 21:03:01 +0530

    leave tracker app basic setup

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl underline bg-slate-700 text-white p-8">
+        Leave Tracker App
+      </h1>
+    </>
+  );
+}
+
+export default App;
