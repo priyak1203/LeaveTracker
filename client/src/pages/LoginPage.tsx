f37433670b81958afe20aa3d8108ee8ec80453d6
Priydarshini Karanth <kpriya1203@gmail.com>
2024-10-25 15:24:57 +0530

    shadcn ui setup and test using button

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,9 +1,14 @@
+import { Button } from '@/components/ui/button';
+
 function LoginPage() {
   return (
     <div>
       <h1 className="text-3xl  bg-slate-500 text-white p-8 text-center">
         Leave Tracker App Login Page
       </h1>
+      <Button size="lg" className="m-20">
+        Login
+      </Button>
     </div>
   );
 }
