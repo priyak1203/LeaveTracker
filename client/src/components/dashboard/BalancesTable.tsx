0dc842338f59fd6f4d5a488f2893784fb40c7f67
Priydarshini Karanth <kpriya1203@gmail.com>
2024-10-27 15:19:16 +0530

    balances page ui

--- client/src/components/dashboard/BalancesTable.tsx
+++ client/src/components/dashboard/BalancesTable.tsx
@@ -1,3 +1,5 @@
+import { IoPencil } from 'react-icons/io5';
+
 import {
   Table,
   TableBody,
@@ -6,23 +8,97 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
+import TableWrapper from '@/components/globals/TableWrapper';
+import { balancesData } from '@/utils/mockData';
+import { Badge } from '@/components/ui/badge';
+
+type BalanceRowTypes = {
+  title: string;
+  values: string[];
+};
+
+const BalanceRowHead = ({ title, values }: BalanceRowTypes) => {
+  return (
+    <TableHead className="text-center">
+      {title}
+
+      <TableRow className="border-none block">
+        {values.map((value, index) => (
+          <TableHead key={index}>{value}</TableHead>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+};
 
 function BalancesTable() {
+  const balanceCategories = [
+    { title: 'ANNUAL', values: ['Credit', 'Used', 'Available'] },
+    { title: 'FAMILY', values: ['Credit', 'Used', 'Available'] },
+    { title: 'HEALTH', values: ['Credit', 'Used', 'Available'] },
+    { title: 'MATERNITY', values: ['Credit', 'Used', 'Available'] },
+    { title: 'PATERNITY', values: ['Credit', 'Used', 'Available'] },
+    { title: 'STUDY', values: ['Credit', 'Used', 'Available'] },
+    { title: 'UNPAID', values: ['Credit', 'Used', 'Available'] },
+  ];
+
+  const renderBalaceRows = (balanceData: any) => {
+    const categories = [
+      'annual',
+      'family',
+      'health',
+      'maternity',
+      'paternity',
+      'study',
+      'unpaid',
+    ];
+
+    return categories.map((category) => (
+      <TableCell key={category}>
+        <TableRow>
+          {['Credit', 'Used', 'Available'].map((type, index) => (
+            <TableCell key={index} className="w-[100px]">
+              {balanceData[`${category}${type}`]}
+            </TableCell>
+          ))}
+        </TableRow>
+      </TableCell>
+    ));
+  };
+
   return (
-    <div>
+    <TableWrapper title="All User Balances">
       <Table>
-        <TableHeader>
+        <TableHeader className="whitespace-nowrap">
           <TableRow>
-            <TableHead>head</TableHead>
+            <TableHead className="text-center">Edit</TableHead>
+            <TableHead className="text-center">User</TableHead>
+            <TableHead className="text-center">Year</TableHead>
+            {balanceCategories.map((category, index) => (
+              <BalanceRowHead
+                key={index}
+                title={category.title}
+                values={category.values}
+              />
+            ))}
           </TableRow>
         </TableHeader>
-        <TableBody>
-          <TableRow>
-            <TableCell>cell</TableCell>
-          </TableRow>
+        <TableBody className="whitespace-nowrap text-center">
+          {balancesData.map((balance) => (
+            <TableRow key={balance.id}>
+              <TableCell>
+                <IoPencil />
+              </TableCell>
+              <TableCell>{balance.name}</TableCell>
+              <TableCell>
+                <Badge>{balance.year}</Badge>
+              </TableCell>
+              {renderBalaceRows(balance)}
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
-    </div>
+    </TableWrapper>
   );
 }
 
