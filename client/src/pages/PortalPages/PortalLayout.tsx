5951bdc688487df28e1b655902b78a442f17b560
Priydarshini Karanth <kpriya1203@gmail.com>
2024-11-05 23:55:24 +0530

    portal pages layout

--- client/src/pages/PortalPages/PortalLayout.tsx
+++ client/src/pages/PortalPages/PortalLayout.tsx
@@ -1,9 +1,17 @@
+import Header from '@/components/globals/Header';
+import Sidebar from '@/components/globals/Sidebar';
 import { Outlet } from 'react-router-dom';
 
 function PortalLayout() {
   return (
     <>
-      <Outlet />
+      <div className="min-h-screen bg-slate-100 dark:bg-black">
+        <Sidebar />
+        <div className="sm:ml-[6rem]">
+          <Header />
+          <Outlet />
+        </div>
+      </div>
     </>
   );
 }
