6cd2a4672bb1bf9b8cc2fe712e32cddcf109b377
Priydarshini Karanth <kpriya1203@gmail.com>
2024-11-09 20:21:13 +0530

    register user controller function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,14 +1,31 @@
+import { StatusCodes } from 'http-status-codes';
+import { BadRequestError } from '../errors/customErrors.js';
 import { validateRegisterUserInput } from '../middlewares/validation.js';
 import User from '../models/userModel.js';
+import { hashPassword } from '../utils/passwordUtils.js';
 
 // Register User
 export const register = async (req, res) => {
-  const { name, email, password, phone, lastName } = req.body;
-
+  // validating inputs
   const validData = validateRegisterUserInput(req.body);
-  console.log(validData);
 
-  res.status(201).json({ data: rawdata });
+  const { email, password } = validData;
+
+  // check if the user already registered
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    throw new BadRequestError('User already registered');
+  }
+
+  const isFirstAccount = (await User.countDocuments()) === 0;
+  validData.role = isFirstAccount ? 'admin' : 'user';
+
+  // hash the password
+  const hashedPassword = await hashPassword(password);
+  validData.password = hashedPassword;
+
+  await User.create(validData);
+  res.status(StatusCodes.CREATED).json({ msg: 'user registered' });
 };
 
 // Login User
---
+++ server/errors/customErrors.js
@@ -0,0 +1,9 @@
+import { StatusCodes } from 'http-status-codes';
+
+export class BadRequestError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'BadRequestError';
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
